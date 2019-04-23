99cd388e8c3c00a77376e0211b4d89fadaf6d18b
iamarjun <arjunmanoj1995@gmail.com>
2019-04-23 16:11:05 +0530

    Initial commit & base setup which can be reused for any application using dagger.

---
+++ app/src/main/java/com/example/daggerandroid/BaseApplication.kt
@@ -0,0 +1,15 @@
+package com.example.daggerandroid
+
+import com.example.daggerandroid.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+class BaseApplication: DaggerApplication() {
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication>? {
+        return DaggerAppComponent.builder()
+            .application(this)
+            .build()
+    }
+
+}
